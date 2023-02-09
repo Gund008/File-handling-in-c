efd3a1f0dd2ace1ac96a91bfd569ea5f96057fa9
Sukanya <106397577+Gund008@users.noreply.github.com>
2023-02-09 22:24:42 +0530

    Add files via upload

---
+++ CommandArgument.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(int argc,char *argv[])
+{
+    printf("Number of Arguments are:%d\n",argv);
+
+    printf("Name of Arguments is:%s\n",argv[0]);
+    
+    return 0;
+
+}
